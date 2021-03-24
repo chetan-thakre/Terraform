916a81059f90c2afbacaf93ae8c5659c9bd2555d
chetan thakre <thakrechetan121@gmail.com>
2021-03-24 09:48:59 +0000

    main.tf added

---
+++ main.tf
@@ -0,0 +1,37 @@
+resource "aws_iam_user" "tarun" {
+  name = "tarun"
+
+  tags = {
+    Name = "terraform"
+  }
+}
+
+
+resource "aws_iam_policy" "allow-ec2" {
+  name        = "allow-ec2"
+  description = "My test policy"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Action": "ec2:*",
+            "Resource": "*",
+            "Effect": "Allow",
+            "Condition": {
+                "StringEquals": {
+                    "ec2:Region": "us-west-1"
+                }
+            }
+        }
+    ]
+})
+}
+
+
+resource "aws_iam_user_policy_attachment" "test-attach" {
+  user       = "tarun"
+  policy_arn = "arn:aws:iam::047437344617:policy/allow-ec2"
+}
